b0be1879dd7cf0f74a4a424ca6c7fbb37b2bf674
Vitalii Honchar <weaxme@gmail.com>
2026-05-07 16:58:02 +0200

    feat: wire binary entrypoints

--- cmd/agentd/main.go
+++ cmd/agentd/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	cliapp "agentd/internal/agentd/app"
+	"agentd/internal/agentd/config"
+	"agentd/internal/agentd/infra/httpclient"
+)
+
 func main() {
+	cfg, err := config.FromEnv()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	client, err := httpclient.New(cfg)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	cmd := cliapp.NewRootCommand(cliapp.RootOptions{
+		Config:        cfg,
+		Client:        client,
+		ExecuteClient: client,
+		StopClient:    client,
+		QueryClient:   client,
+	})
+	if err := cmd.ExecuteContext(context.Background()); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
